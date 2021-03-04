fedc84a5e30bcb2c5c1bb8a5406c50396cab50a4
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2021-03-04 23:56:54 +0000

    Automatically backed up by Learn

---
+++ lib/game.rb
@@ -0,0 +1,14 @@
+# The Game class is the main model of the application and represents singular instance of a Tic-tac-toe session 
+
+# A game has one board through it board property
+
+# A game has two players stored in a player_1 and player_2 property
+
+#Board and Player do not directly relate to the Game but do collaborate with each other through arguments
+
+# Beyond providing relationships with player and a board, the Game instance must also provide the basic game runtime and logic.
+
+# These methods relate to the state of the game such as #current_player, #won and #winner.
+
+# The other methods relate to managing a game like #start, #play and #turn.
+
---
+++ lib/players/computer.rb
@@ -0,0 +1,3 @@
+# Define a class Players::Computer that represents a computer player of Tic-tac-toe
+
+# Implement a move method that acceots a board and returns the move the comouter wants to make in the form of a 1-9 string.
